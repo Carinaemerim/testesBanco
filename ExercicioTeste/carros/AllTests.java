7fab93a6e3c4cdbb8f1b9289c8d868af6bd7309b
Aluno <Aluno@200.17.90.11>
2017-08-23 11:45:59 -0300

    Exercicios teste

---
+++ ExercicioTeste/carros/AllTests.java
@@ -0,0 +1,11 @@
+package carros;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CarroTest.class, MotorTest.class, testaCarroTest.class })
+public class AllTests {
+
+}
--- src/main/java/br/edu/ifrs/canoas/lpoo2/aula01/banco/Cliente.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifrs.canoas.lpoo2.aula01.banco;
-
-public class Cliente {
-	String nome;
-	String sobrenome;
-	String cpf;
-
-}
